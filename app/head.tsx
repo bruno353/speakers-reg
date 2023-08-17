63835afa0dab63097e5500463c9f14342bfb9140
Bruno Santos <brunolsantos152@gmail.com>
2023-08-17 00:05:02 -0300

    feat: new type of registration

--- app/head.tsx
+++ app/head.tsx
@@ -4,7 +4,7 @@ export default function Head() {
       <title>L3A speakers</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Speakers registration calendly" />
-      <link rel="icon" href="/images/l3a-new.ico" />
+      <link rel="icon" href="/images/l3a-transparent.ico" />
     </>
   );
 }
